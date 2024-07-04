95ea8b557f4a6a702a234cec8117027f95aad2a9
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-04 10:25:47 +0000

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,16 +1,16 @@
-#!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for the given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    if response.status_code == 200:
-        data = response.json().get('data')
-        if data:
-            return data.get('subscribers', 0)
-    return 0
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
+        return 0
+
