4585ad61f0c754ee2975af8dd06c66d7f8631bbb
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-04 10:29:18 +0000

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
+"""Return number of subscribers for a given subreddit"""
 import requests
 
 def number_of_subscribers(subreddit):
+    """Return the number of subscribers """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
 
