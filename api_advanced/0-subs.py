766e01459e0fe496d5c755d5025666fc29096267
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-04 11:05:06 +0000

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,15 +5,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return the number of subscribers """
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
-
+    url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return response.json().get('data') \
+            .get('subscribers')
+    return 0
