9c88867d13d4b02024f8bc93176d5e8e31c18e85
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-06 16:28:37 +0000

    API advanced

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -2,22 +2,28 @@
 """DOC"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """DOC"""
-    reddit_url = "https://www.reddit.com/r/{}/about.json" \
-        .format(subreddit)
-
-    header = {'User-agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url,
-                            headers=header
-                            )
-
-    if response.status_code == 200:
-        data = response.json()['data']
-        subs = data['subscribers']
-        return subs
-    return 0
+    """Define the base URL for the Reddit API"""
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    """Define the custom User-Agent to avoid "Too Many Requests" error"""
+    headers = {'User-Agent': 'reddit-subscriber-counter/0.1'}
+    
+    try:
+        """Make a GET request to the Reddit API"""
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        """Check if the response status code is 200 (OK)"""
+        if response.status_code == 200:
+            """Parse the JSON response"""
+            data = response.json()
+            """Return the number of subscribers"""
+            return data['data']['subscribers']
+        else:
+            """If the status code is not 200, return 0"""
+            return 0
+    except Exception:
+        """If there is any exception (like network issues), return 0"""
+        return 0
 
 """Test cases"""
 if __name__ == '__main__':
