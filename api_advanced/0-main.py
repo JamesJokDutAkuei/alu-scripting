ec01180ab0a9d8e84e1143e6c524e12a11ecb790
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-06 17:01:07 +0000

    API advanced

--- api_advanced/0-main.py
+++ api_advanced/0-main.py
@@ -2,11 +2,11 @@
 """
 0-main
 """
-if __name__ == '__main__':
-    import sys
+import sys
 
+if __name__ == '__main__':
+    number_of_subscribers = __import__('0-subs').number_of_subscribers
     if len(sys.argv) < 2:
         print("Please pass an argument for the subreddit to search.")
     else:
-        number_of_subscribers(sys.argv[1])
-        print("OK")
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
