e6809c727861c7a51c16870baf58d71412b20183
JamesJokDutAkuei <j.akuei@alustudent.com>
2024-07-06 14:57:10 +0000

    API advanced

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""DOCS"""
-import requests
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """DOCS"""
-    reddit_url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(reddit_url, headers=headers, allow_redirects=False)
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
+    import requests
 
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        children = data.get('children', [])
-        if not children:
-            print(None)
-        for post in children[:10]:
-            print(post['data']['title'])
+    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
+                            .format(subreddit),
+                            headers={"User-Agent": "My-User-Agent"},
+                            allow_redirects=False)
+    if sub_info.status_code >= 300:
+        print('None')
     else:
-        print(None)
+        [print(child.get("data").get("title"))
+         for child in sub_info.json().get("data").get("children")]
